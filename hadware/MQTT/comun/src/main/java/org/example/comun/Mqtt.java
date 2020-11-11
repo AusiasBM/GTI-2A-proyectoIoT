596907b401e5215b3279f2412468776b885f7d78
Ausias <ausiasbm@gmail.com>
2020-11-11 18:50:50 +0100

    .

--- hadware/MQTT/comun/src/main/java/org/example/comun/Mqtt.java
+++
@@ -1,9 +0,0 @@
-package org.example.comun;
-
-public class Mqtt{
-    public static final String TAG = "MQTT";
-    public static final String topicRoot="arduino/out";//Reemplaza jtomas
-    public static final int qos = 1;
-    public static final String broker = "tcp://mqtt.eclipse.org:1883";
-    public static final String clientId = "";
-}
