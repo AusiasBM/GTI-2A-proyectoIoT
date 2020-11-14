9de7fd38fab652d4579c841d36caa30874864329
Ausias <ausiasbm@gmail.com>
2020-11-14 19:48:55 +0100

    Auto stash before merge of "develop" and "origin/develop"

---
+++ hadware/MQTT/comun/src/main/java/org/example/comun/Mqtt.java
@@ -0,0 +1,11 @@
+package org.example.comun;
+
+public class Mqtt {
+    public static final String TAG = "MQTT";
+    public static final String topicRoot="equipo2/practica/";
+    //Reemplaza equipo por el nombre de tu equipo de prácticas
+    public static final int qos = 1;
+    public static final String broker = "tcp://mqtt.eclipse.org:1883";
+    public static final String clientId = "movil";
+//Reemplaza ClientId con un valor diferente
+}
